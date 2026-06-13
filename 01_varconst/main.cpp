8b64f8eef9bf5f6df6a81b83129008cf5db2a296
Surjoy <ssurjoy07@gmail.com>
2026-06-13 21:49:49 +0530

    Add files via upload

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int score;
+    score = 110;
+
+    const int uid = 65656;
+
+    int balance = 500;
+    cout << "welcome to chai with cpp 1" << endl;
+
+    cout << "welcome to chai with cpp 2" << endl;
+
+    cout << "welcome to chai with cpp 3" << endl;
+    return 0;
+}
---
+++ 01_varconst/prectice.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char Grade = 'b';
+    cout << Grade ;
+ ;
+    return 0;
+}
